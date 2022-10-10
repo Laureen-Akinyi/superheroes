21c35e7a950d7fc4dcc8a51b556cc143fd815099
laureen <laureen.akinyi@student.moringaschool.com>
2022-10-10 18:03:21 +0300

    added relationships

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,2 +1,9 @@
 class HeroPower < ApplicationRecord
+    validates :strength, inclusion: {
+        in: ['Strong', 'Weak', 'Average'],
+        message: "%{value} is not available"    
+    }
+    #  validates :strength, case_sensitive: false
+    belongs_to :power 
+    belongs_to :hero  
 end
