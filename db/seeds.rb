48a5147a2405910137a43fe72c2790c3f0130572
laureen <laureen.akinyi@student.moringaschool.com>
2022-10-10 19:10:42 +0300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,79 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Starting..."
+Hero.create(
+    name: "Simmy",
+    super_name: "The Simpsons"
+)
+Hero.create(
+    name: "Ricochet",
+    super_name: "The League of Incredible Vegetables"
+)
+Hero.create(
+    name: "Scorch",
+    super_name: "Atomic Blonde"
+)
+Hero.create(
+    name: "Gemstone",
+    super_name: "The Masked Mama"
+)
+Hero.create(
+    name: "Blossom",
+    super_name: "Power Puff Girls"
+)
+puts "More power to us..."
+Power.create(
+    name: "Levitation",
+    description: "The ability to float above ground"
+)
+Power.create(
+    name: "Telepathy",
+    description: "The power to read people's mind."
+)
+Power.create(
+    name: "Empath",
+    description: "The ability to feel people's emotions."
+)
+Power.create(
+    name: "Regeneration",
+    description: "The ability of a person to heal quickly."
+)
+Power.create(
+    name: "Speed",
+    description: "The power to move really really fast."
+)
+puts "HerooooPowere!!!"
+HeroPower.create(
+    strength: "Average",
+    hero_id: 1,
+    power_id: 2
+)
+HeroPower.create(
+    strength: "Weak",
+    hero_id: 3,
+    power_id: 4
+)
+HeroPower.create(
+    strength: "Weak",
+    hero_id: 2,
+    power_id: 5
+)
+HeroPower.create(
+    strength: "Strong",
+    hero_id: 1,
+    power_id: 4
+)
+HeroPower.create(
+    strength: "Average",
+    hero_id: 4,
+    power_id: 2
+)
+HeroPower.create(
+    strength: "Strong",
+    hero_id: 5,
+    power_id: 3
+)
+HeroPower.create(
+    strength: "Average",
+    hero_id: 2,
+    power_id: 1
+)
+puts "Done seeding!!"
